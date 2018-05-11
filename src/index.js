c3c441ba3fc77cbdb1ff9237281f8ff99d7caaa2
Artem Korolev <artkor@live.ru>
2018-05-11 12:15:46 +0300

    extending webpack (taken from react-boilerplate)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 /* eslint-disable prop-types */
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const title = 'My Minimal React Webpack Babel Setup';
+const title = "My Minimal React Webpack Babel Setup";
 
 ReactDOM.render(
   <div>{title}</div>,
-  document.getElementById('app'), // eslint-disable-line no-undef
+  document.getElementById("app"), // eslint-disable-line no-undef
 );
 
 module.hot.accept();
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,70 @@
 /* eslint-disable prop-types */
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
 
 module.exports = {
   entry: [
-    'react-hot-loader/patch',
-    './src/index.js',
+    "react-hot-loader/patch",
+    "./src/index.js",
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: ["babel-loader"],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader'],
+        use: ["babel-loader", "eslint-loader"],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
       },
     ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx'],
+    extensions: ["*", ".js", ".jsx"],
   },
   output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/',
-    filename: 'bundle.js',
+    path: path.join(__dirname, "dist"),
+    publicPath: "/",
+    filename: "bundle.js",
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+      inject: true,
+    }),
+    new UglifyJsPlugin({
+      parallel: true,
+    }),
   ],
   devServer: {
-    contentBase: './dist',
+    contentBase: "./dist",
     hot: true,
   },
 };
